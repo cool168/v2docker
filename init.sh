6453be2f513bdac9eeaf0a1fe9aa8e6a67c1fe5f
cool168 <cool168@users.noreply.github.com>
2016-09-20 16:39:30 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 
-#!/bin/bash
-#Todo
+#!/bin/sh
 
-exec "$@"
+INSTALL_FLAG="/v2ray-client.installed"	
+CONF="/client-config.json"
+
+if [ ! -f "$INSTALL_FLAG" ]; then
+	
+	sed -i "s/LOCAL_PORT/${LOCAL_PORT}/g" $CONF
+	sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" $CONF
+	sed -i "s/SERVER_PORT/${SERVER_PORT}/g" $CONF
+	sed -i "s/SERVER_UUID/${SERVER_UUID}/g" $CONF
+	sed -i "s/SERVER_ALTER_ID/${SERVER_ALTER_ID}/g" $CONF
+	
+	touch $INSTALL_FLAG
+fi
+
+/app/v2ray/v2ray -config $CONF
