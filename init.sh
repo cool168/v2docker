3c41057915600e161e55812610604e612cae65ee
cool168 <cool168@users.noreply.github.com>
2016-09-20 16:49:05 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,9 @@
 
 #!/bin/sh
 
-INSTALL_FLAG="/v2ray-client.installed"	
 CONF="/etc/v2ray/client-config.json"
 
-if [ ! -f "$INSTALL_FLAG" ]; then
+if [ $ENABLE_OUTCFG= "no" ]; then
 	
 	sed -i "s/LOCAL_PORT/${LOCAL_PORT}/g" $CONF
 	sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" $CONF
@@ -12,7 +11,6 @@ if [ ! -f "$INSTALL_FLAG" ]; then
 	sed -i "s/SERVER_UUID/${SERVER_UUID}/g" $CONF
 	sed -i "s/SERVER_ALTER_ID/${SERVER_ALTER_ID}/g" $CONF
 	
-	touch $INSTALL_FLAG
 fi
 
 /usr/bin/v2ray/v2ray -config $CONF
