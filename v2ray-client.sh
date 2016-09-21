cd2a0c55361f5ec3de828e01551064ee1af1af6f
cool168 <cool168@users.noreply.github.com>
2016-09-21 18:24:50 +0800

    Create v2ray-client.sh

---
+++ v2ray-client.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+CONF="/etc/v2ray/client-config.json"
+INSTALL_FLAG="/app/v2ray-client.installed"
+
+if [ ! -f "$INSTALL_FLAG" ]; then
+	touch $INSTALL_FLAG
+fi
+
+if [ $ENABLE_OUTCFG = "no" ]; then
+        cp /app/client-config.json $CONF	
+	sed -i "s/LOCAL_PORT/${LOCAL_PORT}/g" $CONF
+	sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" $CONF
+	sed -i "s/SERVER_PORT/${SERVER_PORT}/g" $CONF
+	sed -i "s/SERVER_UUID/${SERVER_UUID}/g" $CONF
+	sed -i "s/SERVER_ALTER_ID/${SERVER_ALTER_ID}/g" $CONF
+	
+fi
+
+/usr/bin/v2ray/v2ray -config $CONF
