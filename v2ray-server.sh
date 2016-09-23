d59d3679b50582d3556398347f119393b33988a3
cool168 <cool168@users.noreply.github.com>
2016-09-23 08:23:58 +0800

    Update v2ray-server.sh

--- v2ray-server.sh
+++ v2ray-server.sh
@@ -9,12 +9,11 @@ fi
 
 if [ $ENABLE_OUTCFG = "no" ]; then
         cp /app/server-config.json $CONF	
-	sed -i "s/LOCAL_PORT/${LOCAL_PORT}/g" $CONF
-	sed -i "s/SERVER_ADDR/${SERVER_ADDR}/g" $CONF
 	sed -i "s/SERVER_PORT/${SERVER_PORT}/g" $CONF
 	sed -i "s/SERVER_UUID/${SERVER_UUID}/g" $CONF
 	sed -i "s/SERVER_ALTER_ID/${SERVER_ALTER_ID}/g" $CONF
-	
+
+	mkdir /var/log/v2ray/
 fi
 
 /usr/bin/v2ray/v2ray -config $CONF
